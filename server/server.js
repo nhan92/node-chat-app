1bb2408c9fd4e34ef3d9ed04f9731890866e74c2
Nhan Nguyen <Sandy@sandy-2.local>
2016-12-01 17:20:37 +0700

    reviewed

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const publicPath = path.join(__dirname, '../public');
+const express = require('express');
 // console.log(__dirname + '/../public');
 // console.log();
-const express = require('express');
+
 const port = process.env.PORT || 3000;
 var app = express();
 app.use(express.static(publicPath));
