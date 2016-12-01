645f4e6e34a84c59e4d8fe764d77aceb35945d2e
Nhan Nguyen <Sandy@sandy-2.local>
2016-12-01 17:59:59 +0700

    add connecect and event handler

--- server/server.js
+++ server/server.js
@@ -1,12 +1,22 @@
 const path = require('path');
 const express = require('express');
-
+const http = require('http');
+const socketIO = require('socket.io');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 
 app.use(express.static(publicPath));
+io.on('connection', (socket) => {
+  console.log('new user connected');
 
-app.listen(port, () => {
+  socket.on('disconnect', () => {
+    console.log('user was disconnected');
+  });
+});
+server.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
