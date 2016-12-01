4d60ec45cc97b1220d2188f58672d9b48e2d57c9
Nhan Nguyen <Sandy@sandy-2.local>
2016-12-01 17:23:34 +0700

    updated from author

--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
 const path = require('path');
-const publicPath = path.join(__dirname, '../public');
 const express = require('express');
-// console.log(__dirname + '/../public');
-// console.log();
 
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
+
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-  console.log(`Server is up on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
